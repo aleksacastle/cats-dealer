ca2a8d85cd3196b283e8f38e9449896a11622a8a
Aleksandra Klochko <aleksandra@streamit.eu>
2019-09-26 23:09:44 +0300

    Add Mandatory methods module

---
+++ lib/mandatory_methods.rb
@@ -0,0 +1,11 @@
+module MandatoryMethods
+  extend ActiveSupport::Concern
+
+  class_methods do
+    def mandatory_methods(*names)
+      names.each do |name|
+        define_method(name) { raise NotImplementedError, "Method `#{name}` has to be implemented!" }
+      end
+    end
+  end
+end
